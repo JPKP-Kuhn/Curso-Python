38e13900936db92e1f7d3a3a8f06e988ce927501
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2023-03-11 00:15:07 -0300

    Básico

---
+++ Módulos do Python/aleatório_emoji.py
@@ -0,0 +1,8 @@
+import random
+import emoji
+num = random.random()
+print(num)
+num =random.randint(1, 10) #entre 1 e 10
+print(num)
+print(emoji.emojize('Python é :thumbs_up:'))
+print(emoji.emojize('Olá Mundo 🌎'))
