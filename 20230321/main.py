a6df1419fccf17179842d919430ee813e563a4f0
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2023-03-25 15:06:54 -0300

    serviço feito

--- 20230321/main.py
+++ 20230321/main.py
@@ -5,10 +5,19 @@ import CaixaAcoplada as CA
 import servico as SE
 
 if __name__ == "__main__":
-	Acao = SE.servico()
+
 	Vaso = CA.CaixaAcoplada()
+	Acao = SE.servico(Vaso)
 	Vaso._encher()
 	Acao._servico()
-	for i in range(3):
-		Vaso._descarga()
+
+	continuar = input('Deseja continuar? S/n ').upper()
+	while continuar == "S":
+		Acao._servico()
+		continuar = input('Deseja continuar? S/n ').upper()
 	print("Número de vezes que a descarga foi usada: ", Vaso._alavanca._contador)
+
+'''
+    for i in range(3):
+        print('Número de descargasa dadas: ', Vaso._alavanca._contador)
+ '''
--- 20230321/servico.py
+++ 20230321/servico.py
@@ -1,13 +1,13 @@
 #coding: utf-8
+import CaixaAcoplada as CA
 
 if __name__ == "__main__":
     print('Só pra saber qual foi o serviço \n não precisa levar a sério, só brincadeira, kkkk')
 
 class servico():
-    def __init__(self):
-        
-        self._servico1 = 1
-        self._servico2 = 2
+    def __init__(self, vaso):
+
+        self._CaixaAcoplada = vaso
     
     def _servico(self):
         decisao = input('Número do serviço que foi feito: ')
@@ -15,5 +15,8 @@ class servico():
             print('Dê a descarga e feche a tampa')
         else:
             print('Depressa! Dê a descarga e feche a tampa!')
-
+        self._CaixaAcoplada._descarga()
+        if self._CaixaAcoplada._nivel_agua == self._CaixaAcoplada._nivel_maximo:
+            self._CaixaAcoplada._encher()
+        
 
