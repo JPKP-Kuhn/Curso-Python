ba54f3ff2dedd1c234bff85e6bf4e48074ba7e10
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2023-05-01 13:21:50 -0300

    correções

--- IFSC_python/20230425/CaixaAcopladaSemImagem/AnimacaoCaixa/Codigo/PygameControl.py
+++ IFSC_python/20230425/CaixaAcopladaSemImagem/AnimacaoCaixa/Codigo/PygameControl.py
@@ -9,7 +9,7 @@ class PygameCtrl(CA.CaixaAcoplada):
     def __init__(self):
         CA.CaixaAcoplada.__init__(self)
         #Metodos pygame
-        pyg.init() #Inicia o pygmae
+        pyg.init() #Inicia o pygamae
         self._TamTela = (680, 480)
         self._CorFundo = (220, 220, 220)
         self._BIF = "../Imagens/CaixaAcoplada.png"
@@ -19,23 +19,25 @@ class PygameCtrl(CA.CaixaAcoplada):
         self.ContTempoSeg = 0
         self.ContTempoAcumulado = 0
 
-        self._dados_Tela()
         self._Screen = pyg.display.set_mode(self._TamTela, 0, 32)
+        self._dados_Tela()
         self._BackImage=pyg.image.load(self._BIF).convert()
         self._pygame_loop()
 
     def _dados_Tela(self):
         
-        self.tick = self.clock.tick() #Faz a 1º marcação de tempo. Inicia o relógio.
-        
+        self.tick = self._clock.tick() #Faz a 1º marcação de tempo. Inicia o relógio.
+       
         #Criação do relógio
         self._RetanguloRelogio = pyg.draw.rect(self._Screen, (255,255,255), pyg.Rect((410, 405),(240,40)))
         FontTempoOperacao = pyg.font.SysFont("Arial", 15)
         self.ContTempoAcumulado+=self.tick
+        print(self.ContTempoSeg)
         self.ContTempoSeg = self.ContTempoAcumulado/1000 #Marca o tempo em segundos
+        self.ContTempoMin = (self.ContTempoAcumulado/1000)/60
         self.TempoMin =1
         self.TempoSeg = 1
-        TextoTmepo = "{0:02d}:{1:02d}".format(int(self.ContTempoSeg), int(self.ContTempoSeg))
+        TextoTempo = "{0:02d}:{1:02d}".format(int(self.ContTempoMin), int(self.ContTempoSeg))
         TextoTempo = FontTempoOperacao.render(TextoTempo, True, (0,0,0))
         self._Screen.blit(TextoTempo, (440,250))
 
