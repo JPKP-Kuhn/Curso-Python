b4abab62516259f559ae608bb06f4204d4657631
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2023-07-25 10:35:05 -0300

    funções parte 2

--- Mundo 3/funcoes/pratica.py
+++ Mundo 3/funcoes/pratica.py
@@ -1,4 +1,5 @@
 #Utilizar funções pra uma rotina
+'''
 def mostralinha():
     print('-' * 30)
 
@@ -45,3 +46,114 @@ valores = [6, 3, 4, 2, 7, 1, 9]
 print(valores)
 dobra(valores)
 print(valores)
+'''
+#Funções parte 2
+
+#Interactive help
+help(print) #Mostra o manual da função print
+print(input.__doc__) #Mostra o manual da função input
+#para sair é quit
+
+#Docstrings
+#É a documentação de uma função, uma string de documentação
+#Toda função deve ter uma docstring, as internas do python tem, com o help
+
+def contador(i, f, p):
+    '''
+    DOCSTRING
+    -> Faz uma contagem e mostra na tela
+    i = início da contagem
+    f = fim da contagem
+    p = passo da contagem
+    return: sem retorno
+    '''
+    c = i
+    while c <= f:
+        print(f'{c} ', end=' ')
+        c += p
+    print('FIM!')
+
+contador(2, 10, 2)
+help(contador) #Mostra a docstring da função
+
+#Parâmetros opcionais
+def somar(a, b, c = 0): #C é opcional, se não for passado, vai valer 0
+    '''
+    Faz a soma de três valores e mostra o resultado na tela
+    '''
+    s = a+b+c
+    print(f'A soma vale {s}')
+
+somar(3, 2, 5)
+somar(8, 4)
+
+#Dá pra colocar todos os parâmetros como opcionais
+def soma(a=0, b=0, c=0):
+    s = a+b+c
+    print(f'A soma vale {s}')
+soma()
+
+#Escopo de variáveis
+#Escopo é o local onde a variável vai existir e onde ela vai deixar de existir
+
+
+def teste():
+    x = 8 #Variável local
+    print(f'Na função teste, n vale {n}')
+    print(f'Na função teste, x vale {x}')
+
+#Program aprincipal
+n = 2 #Variável global
+print(n)
+teste()
+#print(f'No programa principal, x vale {x}') #Vai dar erro, pois x é local
+
+def local(b):
+    global a #Agora a é global
+    a = 8 #Variável local
+    b += 4
+    c = 2
+    print(f'Valor de a {a}')
+    print(f'Valor de b {b}')
+    print(f'Valor de c {c}')
+
+a = 5 #a global
+local(a)
+print(f'Valor de a {a}')
+
+#Retorno de valores return
+
+def somar(a=0, b=0, c=0):
+    s = a+b+c
+    return s #Retorna o valor de s
+
+r1 = somar(3, 2, 5)
+r2 = somar(2, 2)
+r3 = somar(6)
+print(f'Os resultados foram {r1}, {r2} e {r3}')
+
+#Prática
+def fatorial(num=1):
+    f = 1
+    for c in range(num, 0, -1):
+        f *= c
+    return f
+
+n = int(input('Digite um número: '))
+print(f'O fatorial de {n} é igual a {fatorial(n)}')
+f1 = fatorial(5)
+f2 = fatorial(4)
+f3 = fatorial()
+print(f'Os resultados são {f1}, {f2} e {f3}')
+
+def par(n=0):
+    if n%2 == 0:
+        return True
+    else:
+        return False
+    
+num = int(input('Digite um número: '))
+if par(num):
+    print('É par')
+else:
+    print('Não é par')
