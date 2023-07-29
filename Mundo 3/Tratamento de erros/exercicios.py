60f1482fa4da5fe460b12c07586392f0b63da382
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2023-07-28 23:07:21 -0300

    correcoes

--- Mundo 3/Tratamento de erros/exercicios.py
+++ Mundo 3/Tratamento de erros/exercicios.py
@@ -1,25 +1,33 @@
 #Ex1 função leiaInt
 
 def leiaInt(msg):
-    ok = True
-    while ok:
+    #ok = True
+    while True:
         try:
             num = int(input(msg))
-        except:
+        except(ValueError, TypeError):
             print('\033[0;31mERRO! Digite um número inteiro válido!\033[m')
+            continue
+        except KeyboardInterrupt:
+            print('\033[0;31mERRO! O usuário não informou os dados!\033[m')
+            return 0
         else:
-            ok = False
+            #ok = False
             return num
         
 def leiaFloat(msg):
-    ok = True
-    while ok:
+    #ok = True
+    while True:
         try:
             num = float(input(msg))
-        except:
+        except(ValueError, TypeError):
             print('\033[0;31mERRO! Digite um número real válido!\033[m')
+            continue
+        except KeyboardInterrupt:
+            print('\033[0;31mERRO! O usuário não informou os dados!\033[m')
+            return 0
         else:
-            ok = False
+            #ok = False
             return num
 
 num = leiaInt('Digite um número: ')
@@ -27,10 +35,15 @@ real = leiaFloat('Digite um número real: ')
 print(f'Você acabou de digitar o número {num} e {real}')
 
 #Ex2 Acessando o site pudim
-import webbrowser
+import webbrowser, urllib, urllib.request
 webbrowser.open('http://pudim.com.br/')
 webbrowser.open_new_tab('https://lichess.org/')
 
-#Ex3 sistema modularizado
-
+try:
+    site = urllib.request.urlopen('http://pudim.com.br/')
+except urllib.error.URLError:
+    print('O site Pudim não está acessível no momento.')
+else:
+    print('Consegui acessar o site Pudim com sucesso!')
+    print(site.read())
 
